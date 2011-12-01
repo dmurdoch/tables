7ae9f48cb4eb8705e82d7bbbe43a97ca92034efe
murdoch <murdoch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-01 18:57:16 +0000

    Add Literal to support other tabular environments.

--- pkg/tables/R/Hline.R
+++ pkg/tables/R/Hline.R
@@ -1,3 +1,8 @@
+Literal <- function(x) {
+    substitute(Heading(literal)*1*Format((function(x)"")()),
+               list(literal=x))
+}
+
 Hline <- function(columns) {
     if (missing(columns))
     	latex <- "\\hline"
@@ -5,6 +10,5 @@ Hline <- function(columns) {
     	latex <- paste(sprintf("\\cline{%d-%d}", columns, columns), 
     		       collapse=" ")
     latex <- paste(latex, "%")
-    substitute(Heading(latex)*1*Format((function(x)"")()),
-    	       list(latex=latex))
+    Literal(latex)
 }
