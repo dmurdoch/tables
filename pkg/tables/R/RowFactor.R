0f6faf92a040eba69480f3d1ae40e61e96d394b0
murdoch <murdoch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-03 22:01:39 +0000

    Cite Hmisc::latex specifically; add \nopagebreak.

--- pkg/tables/R/RowFactor.R
+++ pkg/tables/R/RowFactor.R
@@ -1,4 +1,5 @@
-RowFactor <- function(x, name = deparse(substitute(x)), spacing=3, space=1) {
+RowFactor <- function(x, name = deparse(substitute(x)), spacing=3, space=1,
+                      nopagebreak = "\\nopagebreak") {
     force(name)
     x <- as.factor(x)
     levs <- levels(x)
@@ -10,8 +11,10 @@ RowFactor <- function(x, name = deparse(substitute(x)), spacing=3, space=1) {
         f <- call("*", call("Heading", as.name(levs[1])), 
                        call("==", x, levs[1]))
         for (i in seq_along(levs)[-1]) {
-            catname <- levs[i]
-            if (spacing[i]) catname <- paste("\\rule[-", space+1, "ex]{0pt}{0pt}", catname, sep="")
+            if (spacing[i]) insert <- paste(nopagebreak, "\\rule[-", space+1, "ex]{0pt}{0pt}", sep="")
+            else if (spacing[i-1]) insert <- ""
+            else insert <- nopagebreak
+            catname <- paste(insert, levs[i], sep="")
             f <- call("+", f, call("*", call("Heading", as.name(catname)), 
                                                 call("==", x, levs[i])))
         }
