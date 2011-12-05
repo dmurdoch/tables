d3510dfe7dbb2ddf6414b01e9a06d38d73a66915
murdoch <murdoch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-05 13:28:47 +0000

    Add doBegin, etc.

--- pkg/tables/R/RowFactor.R
+++ pkg/tables/R/RowFactor.R
@@ -3,16 +3,21 @@ RowFactor <- function(x, name = deparse(substitute(x)), spacing=3, space=1,
     force(name)
     x <- as.factor(x)
     levs <- levels(x)
-    if (is.numeric(spacing)) {
-        spacing <- seq_along(levs) %% spacing == 0
-        spacing[length(spacing)] <- FALSE
+    n <- length(levs)
+    if (is.numeric(spacing) && spacing > 0) {
+        groups <- TRUE
+        spacing <- seq_len(n) %% spacing == 0
+        spacing[n] <- FALSE
+    } else {
+    	groups <- FALSE
+    	spacing <- rep(FALSE, n)
     }
-    if (length(levs)) {
+    if (n) {
         f <- call("*", call("Heading", as.name(levs[1])), 
                        call("==", x, levs[1]))
         for (i in seq_along(levs)[-1]) {
-            if (spacing[i]) insert <- paste(nopagebreak, "\\rule[-", space+1, "ex]{0pt}{0pt}", sep="")
-            else if (spacing[i-1]) insert <- ""
+            if (groups && spacing[i]) insert <- paste(nopagebreak, "\\rule[-", space+1, "ex]{0pt}{0pt}", sep="")
+            else if (!groups || spacing[i-1]) insert <- ""
             else insert <- nopagebreak
             catname <- paste(insert, levs[i], sep="")
             f <- call("+", f, call("*", call("Heading", as.name(catname)), 
