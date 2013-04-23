5ef841256901cb7bf7822c599da9ae9ddc346a00
murdoch <murdoch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-23 13:48:54 +0000

    Better indexing:  now columns can be rearranged.

--- pkg/tables/R/subset.R
+++ pkg/tables/R/subset.R
@@ -15,38 +15,31 @@
 	rattrs$dim <- dim(rattrs$justification)
     	
 	# This is the tricky bit
-	rows <- seq_len(nrow(rowLabels))
-	keep <- rows %in% rows[i]
-	prev <- rep(NA, ncol(rowLabels))
-	for (k in rows) {
-		rowLabels[k,] <- ifelse(is.na(rowLabels[k,]), prev, rowLabels[k,])
-		if (keep[k]) 
-		    prev <- rep(NA, ncol(rowLabels))
-		else
-		    prev <- ifelse(is.na(rowLabels[k,]), prev, rowLabels[k,])
-	}
-	rowLabels <- rowLabels[i,, drop=FALSE]
-	rattrs$dimnames <- dimnames(rowLabels)
+	orig <- row(rowLabels)
+	for (k in seq_len(nrow(orig))[-1]) 
+	    orig[k,] <- ifelse(is.na(rowLabels[k,]), orig[k-1,], orig[k,])
+	orig <- orig[i,, drop=FALSE]
+	rowLabels <- rowLabels[cbind(c(orig), c(col(orig)))]
+	dim(rowLabels) <- dim(orig)
+	for (k in seq_len(nrow(orig))[-1])
+	    rowLabels[k,] <- ifelse(orig[k,] == orig[k-1,], NA, rowLabels[k,])
+	rattrs$dimnames[1] <- list(NULL)
 	
 	# Do the same for column labels
 	colLabels <- attrs$colLabels
 	cattrs <- attributes(colLabels)
 	cattrs$justification <- cattrs$justification[,j, drop=FALSE]
 	cattrs$dim <- dim(cattrs$justification)
-	
-	cols <- seq_len(ncol(colLabels))
-	keep <- cols %in% cols[j]
-	prev <- rep(NA, nrow(colLabels))
-	for (k in cols) {
-		colLabels[,k] <- ifelse(is.na(colLabels[,k]), prev, colLabels[,k])
-		if (keep[k])
-		    prev <- rep(NA, nrow(colLabels))
-		else
-		    prev <- ifelse(is.na(colLabels[,k]), prev, colLabels[,k])
-	}
-	colLabels <- colLabels[,j, drop=FALSE]
-	cattrs$dimnames <- dimnames(colLabels)
-	
+	orig <- col(colLabels)
+	for (k in seq_len(ncol(orig))[-1]) 
+	    orig[,k] <- ifelse(is.na(colLabels[,k]), orig[,k-1], orig[,k])
+	orig <- orig[,j, drop=FALSE]
+	colLabels <- colLabels[cbind(c(row(orig)), c(orig))]
+	dim(colLabels) <- dim(orig)
+	for (k in seq_len(ncol(orig))[-1])
+	    colLabels[,k] <- ifelse(orig[,k] == orig[,k-1], NA, colLabels[,k])
+	cattrs$dimnames[2] <- list(NULL)
+
 	# Put it all back together
 	attributes(rowLabels) <- rattrs
 	attrs$rowLabels <- rowLabels
