8041ac2e7fed618e974b4c6a44e68d9626c091d2
Duncan Murdoch <murdoch.duncan@gmail.com>
2024-11-11 15:06:36 -0500

    reformat

--- R/options.R
+++ R/options.R
@@ -11,7 +11,7 @@ CSSdefault <- '<style>
 #ID .left   { text-align:left; }
 #ID .center { text-align:center; }
 #ID .right  { text-align:right; }
-#ID .Rtable, #ID .Rtable thead { 
+#ID .Rtable, #ID .Rtable thead {
   border-collapse: collapse;
   border-style: solid;
   border-width: medium 0;
@@ -26,75 +26,83 @@ CSSdefault <- '<style>
 '
 
 table_options <- local({
-    opts <- list(justification="c",
-    		 rowlabeljustification="l",
-    		 tabular="tabular",
-    		 toprule="\\hline",
-    		 midrule="\\hline",
-    		 bottomrule="\\hline",
-    		 titlerule=NULL,
-    		 doBegin=TRUE,
-    		 doHeader=TRUE,
-    		 doBody=TRUE,
-    		 doFooter=TRUE,
-    		 doEnd=TRUE,
-    		 knit_print=TRUE,
-    		 latexleftpad=TRUE,
-    		 latexrightpad=TRUE,
-    		 latexminus=TRUE,
-    		 
-    		 doHTMLheader=FALSE,
-    		 doCSS=FALSE,
-    		 doHTMLbody=FALSE,
-    		 
-    		 CSS=CSSdefault,
-    		 HTMLhead=HTMLheaderdefault,
-    		 HTMLbody="<body>\n",
-    		 HTMLattributes='class="Rtable"',
-		 HTMLcaption=NULL,
-    		 HTMLfooter=NULL,
-    		 HTMLleftpad=FALSE,
-    		 HTMLrightpad=FALSE,
-    		 HTMLminus=FALSE
-    		 )
-    function(...) {
-        args <- list(...)
-        if (!length(args))
-            return(opts)
-        else {
-            if (is.list(args[[1L]])) args <- args[[1L]]
-            result <- opts[names(args)]
-            opts[names(args)] <<- args
-            invisible(result)
-        }
+  opts <- list(
+    justification = "c",
+    rowlabeljustification = "l",
+    tabular = "tabular",
+    toprule = "\\hline",
+    midrule = "\\hline",
+    bottomrule = "\\hline",
+    titlerule = NULL,
+    doBegin = TRUE,
+    doHeader = TRUE,
+    doBody = TRUE,
+    doFooter = TRUE,
+    doEnd = TRUE,
+    knit_print = TRUE,
+    latexleftpad = TRUE,
+    latexrightpad = TRUE,
+    latexminus = TRUE,
+    
+    doHTMLheader = FALSE,
+    doCSS = FALSE,
+    doHTMLbody = FALSE,
+    
+    CSS = CSSdefault,
+    HTMLhead = HTMLheaderdefault,
+    HTMLbody = "<body>\n",
+    HTMLattributes = 'class="Rtable"',
+    HTMLcaption = NULL,
+    HTMLfooter = NULL,
+    HTMLleftpad = FALSE,
+    HTMLrightpad = FALSE,
+    HTMLminus = FALSE,
+    escape = FALSE
+  )
+  function(...) {
+    args <- list(...)
+    if (!length(args))
+      return(opts)
+    else {
+      if (is.list(args[[1L]]))
+        args <- args[[1L]]
+      result <- opts[names(args)]
+      opts[names(args)] <<- args
+      invisible(result)
     }
+  }
 })
 
-booktabs <- function(...) { 
-    save <- table_options(
-                  toprule="\\toprule",
-    		  midrule="\\midrule",
-    		  bottomrule="\\bottomrule",
-    		  titlerule="\\cmidrule(lr)")
-    table_options(...) # This may override some of the changes above
-    save
+booktabs <- function(...) {
+  save <- table_options(
+    toprule = "\\toprule",
+    midrule = "\\midrule",
+    bottomrule = "\\bottomrule",
+    titlerule = "\\cmidrule(lr)"
+  )
+  table_options(...) # This may override some of the changes above
+  save
 }
 
-htmloptions <- function(head=TRUE, table=TRUE, pad=FALSE, ...) {
-    save <- table_options()
-    on.exit(table_options(save))
-    table_options(
-    		 doHTMLheader=head,
-    		 doCSS=head,
-    		 doHTMLbody=head,                 
-    		 doBegin=table,
-    		 doHeader=table,
-    		 doFooter=table,
-    		 doBody=table,
-    		 doEnd=table,
-		 HTMLleftpad=pad,
-		 HTMLrightpad=pad,
-		 HTMLminus=pad)
-    table_options(...) # This may override some of the changes above
-    table_options()
+htmloptions <- function(head = TRUE,
+                        table = TRUE,
+                        pad = FALSE,
+                        ...) {
+  save <- table_options()
+  on.exit(table_options(save))
+  table_options(
+    doHTMLheader = head,
+    doCSS = head,
+    doHTMLbody = head,
+    doBegin = table,
+    doHeader = table,
+    doFooter = table,
+    doBody = table,
+    doEnd = table,
+    HTMLleftpad = pad,
+    HTMLrightpad = pad,
+    HTMLminus = pad
+  )
+  table_options(...) # This may override some of the changes above
+  table_options()
 }
